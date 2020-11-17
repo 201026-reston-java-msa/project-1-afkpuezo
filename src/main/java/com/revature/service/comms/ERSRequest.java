b507a79084c8616945b14796eb5bacd8e4b1f834
afkpuezo <andrewfcurry@gmail.com>
2020-11-17 12:31:08 -0500

    forgot to open new branch

---
+++ src/main/java/com/revature/service/comms/ERSRequest.java
@@ -0,0 +1,15 @@
+/**
+ * This class is used for communicating between the front-end and service layers.
+ * When the user wants/attempts to take an action, the front end will create a
+ * corresponding ERSRequest object and pass it to the service layer to be processed.
+ */
+package com.revature.service.comms;
+
+public class ERSRequest {
+    
+    // enums
+
+    public enum Type{
+        
+    }
+}
