0892673b9bed777cddbf71025988ec62e21067b4
afkpuezo <andrewfcurry@gmail.com>
2020-11-23 14:58:42 -0500

    wrote skeleton of BackEndUtil class; need to implement DAOs to finish it

---
+++ src/main/java/com/revature/service/BackEndUtil.java
@@ -0,0 +1,24 @@
+/**
+ * This class manages the front end's reference to the back end through a
+ * singleton pattern.
+ * 
+ * @author Andrew Curry
+ */
+package com.revature.service;
+
+import com.revature.repository.DAO.interfaces.ReimbursementRequestDAO;
+
+public class BackEndUtil {
+    
+    // class / static variables
+    private static boolean sfInstantiated;
+    private static ServiceFront instance;
+
+    public static ServiceFront getBackEnd(){
+        if (sfInstantiated) return instance;
+        else{
+            // TODO create DAOs, create handlers, create service front
+            return null;
+        }
+    }
+}
