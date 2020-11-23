64c87fa241f9d2db4922cf182e660db7c9495401
afkpuezo <andrewfcurry@gmail.com>
2020-11-23 15:23:50 -0500

    corrected improper switch syntax in ServiceFront

--- src/main/java/com/revature/service/ServiceFront.java
+++ src/main/java/com/revature/service/ServiceFront.java
@@ -61,31 +61,31 @@ public class ServiceFront {
         ERSRequestType type = req.getType();
         switch(type){
 
-            LOG_IN:
+            case LOG_IN:
                 return arh.handleLogIn(req);
-            LOG_OUT:
+            case LOG_OUT:
                 return arh.handleLogOut(req);
-            SUBMIT_REQUEST:
+            case SUBMIT_REQUEST:
                 return mrh.handleSubmitRequest(req);
-            EMPLOYEE_VIEW_PENDING:
+            case EMPLOYEE_VIEW_PENDING:
                 return vrh.handleEmployeeViewPending(req);
-            EMPLOYEE_VIEW_RESOLVED:
+            case EMPLOYEE_VIEW_RESOLVED:
                 return vrh.handleEmployeeViewResolved(req);
-            EMPLOYEE_VIEW_SELF:
+            case EMPLOYEE_VIEW_SELF:
                 return vrh.handleEmployeeViewSelf(req);
-            EMPLOYEE_UPDATE_SELF:
+            case EMPLOYEE_UPDATE_SELF:
                 return mrh.handleEmployeeUpdateSelf(req);
-            APPROVE_REQUEST:
+            case APPROVE_REQUEST:
                 return mrh.handleApproveRequest(req);
-            DENY_REQUEST:
+            case DENY_REQUEST:
                 return mrh.handleDenyRequest(req);
-            VIEW_ALL_PENDING:
+            case VIEW_ALL_PENDING:
                 return vrh.handleViewAllPending(req);
-            VIEW_ALL_RESOLVED:
+            case VIEW_ALL_RESOLVED:
                 return vrh.handleViewAllResolved(req);
-            VIEW_ALL_EMPLOYEES:
+            case VIEW_ALL_EMPLOYEES:
                 return vrh.handleViewAllEmployees(req);
-            MANAGER_VIEW_BY_EMPLOYEE:
+            case MANAGER_VIEW_BY_EMPLOYEE:
                 return vrh.handleManagerViewByEmployee(req);
             default: // should never happen?
                 return new ERSResponse(
