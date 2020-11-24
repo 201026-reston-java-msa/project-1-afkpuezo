57d983a435348577c73b098bd3606d2c2ce0af14
afkpuezo <andrewfcurry@gmail.com>
2020-11-24 11:45:55 -0500

    tweaked HibernateConnectionUtil to have a test mode which uses the special test config file

--- src/main/java/com/revature/repository/Util/HibernateConnectionUtil.java
+++ src/main/java/com/revature/repository/Util/HibernateConnectionUtil.java
@@ -9,13 +9,60 @@ import org.hibernate.cfg.Configuration;
 
 public class HibernateConnectionUtil {
 
-	
-    private static SessionFactory sf 
-            = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-	
+	// constants
+	public static final String CONFIG_FILE_NAME = "hibernate.cfg.xml";
+	public static final String TEST_CONFIG_FILE_NAME = "test_hibernate.cfg.xml";
+
+	// class / static variables
+	private static boolean isInitialized = false;
+	private static boolean isTestMode = false;
+
+    private static SessionFactory sf; 
 	
 	//get a Session from the SessionFactory Method
 	public static Session getSession(){
+
+		if (!isInitialized){ // figure out what kind to make
+
+			String fileName;
+
+			if (isTestMode) fileName = TEST_CONFIG_FILE_NAME;
+			else fileName = CONFIG_FILE_NAME;
+
+			sf = new Configuration().configure(fileName).buildSessionFactory();
+			isInitialized = true;
+		}
+
 		return sf.openSession();
+	}
+
+	/**
+	 * Switches to using the test configuration file. If starting in normal mode and there
+	 * is already a SessionFactory initialized, it will be dropped.
+	 * If already in testMode, does nothing.
+	 */
+	public void enterTestMode(){
+
+		if (!isTestMode && isInitialized){
+			sf.close();
+			sf = null;
+			isInitialized = false;
+		}
+		isTestMode = true;
+	}
+
+	/**
+	 * Switches to using the normal configuration file. If starting in test mode and there
+	 * is already a SessionFactory initialized, it will be dropped.
+	 * If already in normal mode, does nothing.
+	 */
+	public void exitTestMode(){
+
+		if (isTestMode && isInitialized){
+			sf.close();
+			sf = null;
+			isInitialized = false;
+		}
+		isTestMode = false;
 	}
 }
