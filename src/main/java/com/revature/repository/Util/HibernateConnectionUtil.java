fbc2d58c4b0d41abf6079b4d546785b2fc9216a8
afkpuezo <andrewfcurry@gmail.com>
2020-11-24 10:56:17 -0500

    copied HibernateConnectionUtil from class demos

---
+++ src/main/java/com/revature/repository/Util/HibernateConnectionUtil.java
@@ -0,0 +1,20 @@
+/**
+ * Shamelessly taken from one of the provided class demos.
+ */
+package com.revature.repository.Util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateConnectionUtil {
+
+	
+	private static SessionFactory sf = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+	
+	
+	//get a Session from the SessionFactory Method
+	public static Session getSession(){
+		return sf.openSession();
+	}
+}
