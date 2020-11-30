8f351fd02eb130e129a3591f8fcd77baa7d305d5
afkpuezo <andrewfcurry@gmail.com>
2020-11-29 22:26:57 -0500

    skeleton of IndexServlet

---
+++ src/main/java/com/revature/servlets/IndexServlet.java
@@ -0,0 +1,50 @@
+/**
+ * This servlet handles the index page - that is, it determines what actions are available
+ * to the current user and presents them.
+ */
+package com.revature.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.revature.service.BackEndUtil;
+
+public class IndexServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 0L;
+
+    public IndexServlet() {
+        super();
+    }
+
+    /**
+     * DESCRIPTION
+     * 
+     * @param request
+     * @param response
+     * @throws ServletException, IOException
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+
+        request.getRequestDispatcher("index.html").forward(request, response);
+    }
+    
+    /**
+     * DESCRIPTION
+     * 
+     * @param request
+     * @param response
+     * @throws ServletException, IOException
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+		
+	}
+}
