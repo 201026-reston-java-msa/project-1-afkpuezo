60cd20e2e36bf983c3a04bba7a403c04f19a1d26
afkpuezo <andrewfcurry@gmail.com>
2020-11-29 22:19:06 -0500

    folder structure and servlet template

---
+++ src/main/java/com/revature/servlets/ServletTemplate.java
@@ -0,0 +1,49 @@
+/**
+ * DOCSTRING
+ */
+package com.revature.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.revature.service.BackEndUtil;
+
+@SuppressWarnings(value="all")
+public class ServletTemplate extends HttpServlet {
+
+    private static final long serialVersionUID = 0L;
+    
+    public ServletTemplate() {
+        super();
+    }
+
+    /**
+     * DESCRIPTION
+     * 
+     * @param request
+     * @param response
+     * @throws ServletException, IOException
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+		
+    }
+    
+    /**
+     * DESCRIPTION
+     * 
+     * @param request
+     * @param response
+     * @throws ServletException, IOException
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+		
+	}
+}
